aaf3ba180ed9d535f287f09a7495833b9b50d968
Danny Thompson <dannymthompson94@gmail.com>
2021-10-30 14:15:03 -0700

    Initial services package/module setup
    This includes setting up dagger classes and initial setup of the repo layer

--- services/src/main/java/RestaurantRepo.kt
+++ services/src/main/java/RestaurantRepo.kt
@@ -1,2 +1,8 @@
-class RestaurantRepo {
+package com.dthompson.services
+
+import com.dthompson.core.Restaurant
+import io.reactivex.Single
+
+interface RestaurantRepo {
+    fun getRestaurants(query: String, locationString: String): Single<List<Restaurant>>
 }
--- services/src/main/java/com/dthompson/services/di/ServiceModule.kt
+++ services/src/main/java/com/dthompson/services/di/ServiceModule.kt
@@ -1,4 +1,15 @@
 package com.dthompson.services.di
 
+import com.dthompson.api.RestaurantApi
+import com.dthompson.services.DefaultRestaurantRepo
+import com.dthompson.services.RestaurantRepo
+import dagger.Module
+import dagger.Provides
+
+@Module
 class ServiceModule {
+    @Provides
+    fun provideRestaurantRepo(restaurantApi: RestaurantApi): RestaurantRepo {
+        return DefaultRestaurantRepo(restaurantApi)
+    }
 }
