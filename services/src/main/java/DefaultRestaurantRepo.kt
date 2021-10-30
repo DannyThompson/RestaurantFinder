fca8d9ab7dc7a5edf4e0a546fc96c99fdea88de8
Danny Thompson <dannymthompson94@gmail.com>
2021-10-30 14:15:03 -0700

    Initial services package/module setup
    This includes setting up dagger classes and initial setup of the repo layer

--- services/src/main/java/DefaultRestaurantRepo.kt
+++ services/src/main/java/DefaultRestaurantRepo.kt
@@ -1,2 +1,17 @@
-class DefaultRestaurantRepo {
+package com.dthompson.services
+
+import com.dthompson.api.RestaurantApi
+import com.dthompson.core.Restaurant
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+class DefaultRestaurantRepo(val restaurantApi: RestaurantApi): RestaurantRepo {
+
+    override fun getRestaurants(query: String, locationString: String): Single<List<Restaurant>> {
+        return restaurantApi.getRestaurantsForQuery(query, locationString)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+        // todo: add onErrorResumeNext?
+    }
 }
--- services/src/main/java/com/dthompson/services/di/ServiceComponent.kt
+++ services/src/main/java/com/dthompson/services/di/ServiceComponent.kt
@@ -1,4 +1,17 @@
 package com.dthompson.services.di
 
-class ServiceComponent {
+import com.dthompson.api.di.ApiModule
+import com.dthompson.services.RestaurantRepo
+import dagger.Component
+
+@Component(
+    modules = [
+        ApiModule::class,
+        ServiceModule::class
+    ]
+)
+
+interface ServiceComponent {
+
+    fun exposeRestaurantRepo(): RestaurantRepo
 }
