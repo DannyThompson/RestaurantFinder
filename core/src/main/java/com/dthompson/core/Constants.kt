4707233f13558951c87a05c104942043c5ac5c75
Danny Thompson <dannymthompson94@gmail.com>
2021-10-31 01:59:41 -0700

    Cleanup core  module

--- core/src/main/java/com/dthompson/core/Constants.kt
+++ core/src/main/java/com/dthompson/core/Constants.kt
@@ -3,3 +3,5 @@ package com.dthompson.core
 const val BASE_URL = "https://maps.googleapis.com/maps/"
 const val GOOGLE_PLACES_API_KEY = "AIzaSyDQSd210wKX_7cz9MELkxhaEOUhFP0AkSk"
 const val REQUESTED_TYPE = "restaurant" // For now, all we care about is restaurants, so just hard code the type.
+const val QUERY_TYPE_ALL = "QUERY_TYPE_ALL"
+const val QUERY_TYPE_SEARCH = "QUERY_TYPE_SEARCH"
