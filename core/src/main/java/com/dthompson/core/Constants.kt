04735e8621f7e3c2d526dfe0acc1598a209aefcb
Danny Thompson <dannymthompson94@gmail.com>
2021-11-02 15:20:57 -0700

    Adding hardcoded photos URL
    Since this is only used to fetch a photo using Coil, we don't need an @GET method in our api layer

--- core/src/main/java/com/dthompson/core/Constants.kt
+++ core/src/main/java/com/dthompson/core/Constants.kt
@@ -8,3 +8,6 @@ const val QUERY_TYPE_ALL = "QUERY_TYPE_ALL"
 const val QUERY_TYPE_SEARCH = "QUERY_TYPE_SEARCH"
 // Additional fields we want to request when a user clicks on a restaurant.
 const val DETAILS_PHONE_AND_PHOTOS = "photos,formatted_phone_number"
+
+// We always use the same URL to just call Coil.load() on an imageview to set a photo, so just hardcode the url and replace the necessary parameters when needed.
+const val PHOTOS_URL = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=350&maxheight=350&photo_reference={reference}&key={key}"
